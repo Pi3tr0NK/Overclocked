51c4da2d4f1ab00a7494f42a809923f792565d6b
Pietro <pmontella86@gmail.com>
2026-05-16 12:45:29 +0200

    UtenteDAO

---
+++ src/main/java/dao/UtenteDAO.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import model.UtenteBean;
+
+public interface UtenteDAO 
+{
+	public void doSave(UtenteBean u) throws SQLException;
+	
+	public UtenteBean doRetrieveByKey(int id) throws SQLException;
+	
+	public UtenteBean doRetrieveByEmail(String email) throws SQLException;
+	
+	public boolean doUpdate(UtenteBean u) throws SQLException;
+	
+	public UtenteBean checkLogin(String email, String password) throws SQLException;
+	
+	public Collection<UtenteBean> doRetrieveAll() throws SQLException;
+	
+	public boolean updatePassword(int idUtente, String newPassword) throws SQLException;
+}
