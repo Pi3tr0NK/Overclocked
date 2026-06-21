730d3f764c277109248f945897614c4b39ea23bf
Tobia95 <carlinoboss954@gmail.com>
2026-06-21 19:12:41 +0200

    Fix a LoginControl

--- src/main/java/control/common/LoginControl.java
+++ src/main/java/control/common/LoginControl.java
@@ -42,13 +42,6 @@ public class LoginControl extends HttpServlet {
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/common/LoginView.jsp")
-				.forward(request, response);
-	}
-
-	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
